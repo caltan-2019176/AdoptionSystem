1d6a04c139dc6b33041c68e784bf881c9264a60e
caltan-2019176 <caltan-2019176@kinal.edu.gt>
2024-02-09 09:02:42 -0600

    primer commit

---
+++ src/animal/animal.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose'
+
+const animalSchema = mongoose.Schema({
+    nameAnimal:{
+        type: String, 
+        unique: true,
+        required: true
+    }, 
+    typeAnimal:{
+        type: String, 
+        required: true
+    },
+    race:{
+        type: String, 
+        required: true
+    },
+    genre:{
+        type: String, 
+        required: true
+    },
+    paws:{
+        type: String, 
+        required: true, 
+        minLength: 1,
+    },
+    keeper:{
+        type: Schema.ObjectId, ref: "user"
+    }
+})
---
+++ src/user/user.routes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { test, register, login, updatePassword, deleteUser, update } from './user.controller.js';
+
+const api = express.Router();
+
+api.get('/test', test)
+api.post('/register', register)
+api.post('/login', login)
+api.put('/updatePassword', updatePassword)
+api.delete('/deleteUser/:id', deleteUser)
+api.put('/update/:id', update)
+export default api
