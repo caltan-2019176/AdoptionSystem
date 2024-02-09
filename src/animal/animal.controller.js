ab5a0de1f3460ef75d778540f4f4e6d0a38c46ae
caltan-2019176 <caltan-2019176@kinal.edu.gt>
2024-02-09 11:30:10 -0600

    Commit 3, se tienen el delete, agregar, modelo, Buscar

--- src/animal/animal.controller.js
+++ src/animal/animal.controller.js
@@ -1,4 +1,5 @@
 'use strict'
+import { response } from 'express'
 import Animal from './animal.model.js'
 
 
@@ -21,11 +22,45 @@ export const guardarAnimal = async(req, res) =>{
     }
 }
 
-export const obtenerAnimal = async(req, res) =>{
+export const obtenerAnimal = async (req, res = response) => {
+
     try {
-        
+        const totalAnimales = await Animal.find();
+        res.json({ total: totalAnimales });
+
     } catch (error) {
         console.error(error)
-        return res.status(500).send({message: 'Error listing animals', error: error})
+        return res.status(500).send({ message: 'Error listing animals', error: error })
     }
+}
+
+
+export const buscarAnimal = async(req, res) =>{
+    try {
+        let {id} = req.params
+        let animal = await Animal.findOne({_id : id});
+        return res.json({ message: 'Animal find', animal })
+
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ message: 'Error seraching animal', error: error })
+    }
+}
+
+
+export const deleteAnimal = async (req, res) =>{
+    try {
+        
+        let{id} = req.params
+        let deletedAnimal =  await Animal.findOneAndDelete({_id: id})
+        if(!deletedAnimal) return res.status(404).send({message: 'Animal not found and not deleted'})
+        return res.send({message: `Animal ${deletedAnimal.nameAnimal} deleted successfully`})
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ message: 'Error deleting Animal', error: error })
+    }
+
+
 }
--- src/animal/animal.routes.js
+++ src/animal/animal.routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { guardarAnimal, testA } from './animal.controller.js';
+import { buscarAnimal, deleteAnimal, guardarAnimal, obtenerAnimal, testA } from './animal.controller.js';
 const api = express.Router();
 api.get('/testA', testA)
 api.post('/guardarAnimal', guardarAnimal)
-
+api.get('/obtenerAnimal', obtenerAnimal)
+api.get('/buscarAnimal/:id', buscarAnimal)
+api.delete('/deleteAnimal/:id', deleteAnimal)
 
 
 
